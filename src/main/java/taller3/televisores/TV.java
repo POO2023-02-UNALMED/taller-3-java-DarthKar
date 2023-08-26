684fa8ec87c739d266a2e98366f46fbb8958314a
Miguel Angel Peña Marin <138605407+DarthKar@users.noreply.github.com>
2023-08-26 11:32:31 -0500

    Update TV.java

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -24,7 +24,11 @@ public class TV {
 		return marca;
 	}
 	public void setCanal(int canal) {
-		this.canal = canal;
+		if (estado == true) {
+			if(canal>=1&&canal<120) {
+				this.canal=canal;
+			}
+		}
 	}
 	public int getCanal() {
 		return canal;
@@ -36,7 +40,11 @@ public class TV {
 		return precio;
 	}
 	public void setVolumen(int volumen) {
-		this.volumen = volumen;
+		if (estado == true) {
+			if(volumen>=0&&volumen<=7) {
+				this.volumen=volumen;
+			}
+		}
 	}
 	public int getVolumen() {
 		return volumen;
